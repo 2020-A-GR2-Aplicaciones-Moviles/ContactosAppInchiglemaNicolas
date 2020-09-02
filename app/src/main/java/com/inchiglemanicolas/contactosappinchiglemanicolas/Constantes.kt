a77f6be90c0b5361785d09d1ad6dd5b5c26f7180
ShadowNK <inchiglemanicolax@gmail.com>
2020-09-02 14:41:45 -0500

    Laboratorio11

--- app/src/main/java/com/inchiglemanicolas/contactosappinchiglemanicolas/Constantes.kt
+++ app/src/main/java/com/inchiglemanicolas/contactosappinchiglemanicolas/Constantes.kt
@@ -1,6 +1,12 @@
 package com.inchiglemanicolas.contactosappinchiglemanicolas
 
+import com.google.firebase.auth.FirebaseAuth
+
 const val USER_KEY = "USER_KEY"
+const val REMEMBER_KEY = "REMEMBER_KEY"
 const val PASSWORD_KEY = "PASWORD_KEY"
 const val USER_DATA_FILE = "USER_DATA_FILE"
 const val ENC_USER_DATA_FILE = "ENC_USER_DATA_FILE"
+const val PERMISSION_REQUEST_CODE = 100
+
+lateinit var mAuth: FirebaseAuth
---
+++ app/src/main/java/com/inchiglemanicolas/contactosappinchiglemanicolas/ContactModelClass.kt
@@ -0,0 +1,3 @@
+package com.inchiglemanicolas.contactosappinchiglemanicolas
+
+data class ContactoModelClass(var userId:Int, var firstName: String, var lastName: String, var phoneNumber: String, var emailAddress: String)
